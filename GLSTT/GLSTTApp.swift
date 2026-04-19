f9b262b0e3c1872f66cab411b04dd26c785e96c8
Naftali Antebi <97865688+SwiftDevStudent@users.noreply.github.com>
2026-04-19 12:03:10 -0400

    Initial Commit

---
+++ GLSTT/GLSTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  GLSTTApp.swift
+//  GLSTT
+//
+//  Created by Naftali Antebi on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct GLSTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
